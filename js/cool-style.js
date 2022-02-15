c9f35015504c19b3ea803ba50495d4acab1624ce
juanmagal10 <juanmagalvan.24@gmail.com>
2022-02-15 00:29:10 -0300

    subida 2

--- js/cool-style.js
+++ js/cool-style.js
@@ -1,17 +1,24 @@
-const menu = document.getElementById('menu');
-const submenu = document.getElementById('submenu')
-// submenu.addEventListener('mouseover', () => {
-//     submenu.classList.replace('submenu-hidden','submenu')
-    
-// })
+const menu = document.querySelectorAll('.menu');
+const submenu = document.querySelectorAll('.submenu')
 
-// submenu.addEventListener('mouseout', () => {
-//     submenu.classList.add('submenu-hidden')
-//     submenu.classList.remove('submenu')
-// })
 
-menu.addEventListener('click', () => {
-        submenu.classList.toggle('submenu-hidden')
-   
-})
+menu.forEach(i => {
+        if (i.firstElementChild == <br>) {
+              i.firstElementChild.nextSibling.classList.replace('submenu-hidden','submenu-show')
+        } else {
+                i.addEventListener('mouseover', () => {
+                        i.firstElementChild.classList.replace('submenu-hidden','submenu-show')
+                    })
+                i.addEventListener('mouseout', () => {
+                    i.firstElementChild.classList.replace('submenu-show',"submenu-hidden")
+                })
+      }
+        
+});
+
+
+
+
+
+
 
